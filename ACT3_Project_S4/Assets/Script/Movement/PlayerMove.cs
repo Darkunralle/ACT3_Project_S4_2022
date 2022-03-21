9ff704f8f3773b2dc824a2d74d9e801ca29a45eb
Kalion1 <nethys79@gmail.com>
2022-03-21 12:00:11 +0100

    up-21/03/2022-12:00

--- ACT3_Project_S4/Assets/Script/Movement/PlayerMove.cs
+++ ACT3_Project_S4/Assets/Script/Movement/PlayerMove.cs
@@ -17,6 +17,9 @@ public class PlayerMove : MonoBehaviour
     [SerializeField, Tooltip("Multiplicateur de vitesse pour le sprint (1 = vitesse de base) Float ")]
     private float m_speedMulti = 1.5f;
 
+    [SerializeField, Tooltip("Barre d'endurance Float ")]
+    private float m_stamBarre = 100f;
+
     private float m_gravityCalcul = 0;
 
 
@@ -45,6 +48,7 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
+        Debug.Log(m_stamBarre);
 
         // Faire quelque chose pour le sprint a l'arrière
         Vector2 move = playerInput.Player.Move.ReadValue<Vector2>();
@@ -54,18 +58,30 @@ public class PlayerMove : MonoBehaviour
         // Pbm mystique le else s'appel tjrs même si la condition du if est vérifié
         if (m_characterController.isGrounded)
         {
-            Debug.Log("sol");
+            //Debug.Log("sol");
 
             movement = transform.forward * move.y * m_speed;
-
-            if (playerInput.Player.Sprint.IsPressed())
+            
+            ///(m_stamBarre = Mathf.Round(1);) jarrive pas a arrondire la stam </summary>
+            
+            //activaction si input.pressed et stam sup à 0
+            if (playerInput.Player.Sprint.IsPressed() && m_stamBarre > 0)
             {
+                m_stamBarre -= 5 * Time.deltaTime;
                 movement = transform.forward * move.y * (m_speed * m_speedMulti);
             }
+
+            //activation si !input.pressed = rechargement de la stam
+            if (!playerInput.Player.Sprint.IsPressed() && m_stamBarre < 20)
+            {
+                m_stamBarre += 5 * Time.deltaTime;
+            }
             
+            //petit bon
             if (playerInput.Player.Jump.IsPressed())
             {
-                Debug.Log("Jump");
+                movement = transform.forward * move.x * (m_speed * m_speedMulti);
+                //Debug.Log("Jump");
             }
 
         }
@@ -75,7 +91,7 @@ public class PlayerMove : MonoBehaviour
             m_gravityCalcul -= m_gravity;
 
             movement.y = m_gravityCalcul;
-            Debug.Log("Gravity");
+            //Debug.Log("Gravity");
         }
 
         m_characterController.Move(movement * Time.deltaTime);
