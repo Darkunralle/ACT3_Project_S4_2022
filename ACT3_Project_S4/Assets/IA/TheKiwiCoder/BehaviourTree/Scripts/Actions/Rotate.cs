3e0d8007cd11f8c46f47ba55657dd36e671451c7
Kalion1 <nethys79@gmail.com>
2022-03-23 17:37:55 +0100

    up

--- ACT3_Project_S4/Assets/IA/TheKiwiCoder/BehaviourTree/Scripts/Actions/Rotate.cs
+++ ACT3_Project_S4/Assets/IA/TheKiwiCoder/BehaviourTree/Scripts/Actions/Rotate.cs
@@ -6,23 +6,27 @@ using TheKiwiCoder;
 public class Rotate : ActionNode
 {
     private GameObject agent;
+    private Transform transform;
     public float duration = 1;
     float startTime;
+
+    public bool updateRotation = false;
 
     protected override void OnStart() {
         startTime = Time.time;
         agent = blackboard.Agent;
+        context.agent.updateRotation = updateRotation;
+
+        transform = agent.GetComponent<Transform>();
     }
 
     protected override void OnStop() {
     }
 
     protected override State OnUpdate() {
-        agent.transform.Rotate(Vector3.up, 50f * Time.deltaTime);
-        if (Time.time - startTime > duration)
-        {
-            return State.Success;
-        }
+
+        transform.Rotate(0,+90f,0);
+
         return State.Running;
     }
 }
