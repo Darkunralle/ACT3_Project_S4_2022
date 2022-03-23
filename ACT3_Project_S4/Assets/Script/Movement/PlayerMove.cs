7c411a8ed929d9742c0c037e9a76e77b91d4ca16
Aymeric <73876993+Darkunralle@users.noreply.github.com>
2022-03-23 17:38:37 +0100

    Update Mouvement
    Affinage de la gravité
    Désactivation du air control
    Ajout du saut
    
    WIP :
    Saut charger
    Désactiver le sprint arrière
    Affinage de la stam
    
    ToDo :
    Mécanique de son

--- ACT3_Project_S4/Assets/Script/Movement/PlayerMove.cs
+++ ACT3_Project_S4/Assets/Script/Movement/PlayerMove.cs
@@ -8,8 +8,11 @@ public class PlayerMove : MonoBehaviour
     [SerializeField, Tooltip("Vitesse du joueur en mètre par seconde")] 
     private float m_speed = 5f;
 
-    [SerializeField, Tooltip("La gravité gravité terrestre : 9,8 m/s²")]
-    private float m_gravity = 9.8f;
+    [SerializeField, Tooltip("La gravité gravité terrestre : -9,8 m/s²")]
+    private float m_gravity = -9.8f;
+
+    [SerializeField, Tooltip("Hauteur du saut en m")]
+    private float m_jumpHeight = 5f;
 
     [SerializeField, Tooltip("Rotation degré par seconde")]
     private float m_rotateSpeed = 45;
@@ -20,7 +23,18 @@ public class PlayerMove : MonoBehaviour
     [SerializeField, Tooltip("Barre d'endurance Float ")]
     private float m_stamBarre = 100f;
 
-    private float m_gravityCalcul = 0;
+    [SerializeField, Tooltip("Sphere invisible pour détecter la collision avec le bloc (Mettre l'empty Groundcheck)")]
+    private Transform m_groundCheck;
+
+    [SerializeField, Tooltip("Float gerant le radius de la sphere GroundCheck")]
+    private float m_groundCheckRange = 0.4f;
+
+    [SerializeField, Tooltip("LayerMask du sol")]
+    private LayerMask m_groundMask;
+
+    private bool m_isGrounded;
+
+    private Vector3 m_gravityEffect;
 
 
     private Vector3 movement = new Vector3(0, 0, 0);
@@ -48,22 +62,30 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(m_stamBarre);
+        // Crée une sphere invisible et check si elle colide avec un layer "Ground"
+        m_isGrounded = Physics.CheckSphere(m_groundCheck.position, m_groundCheckRange, m_groundMask);
+
+        // Reset de la force de gravité au sol (Pour ne pas qu'elle augmente de manière idiote une fois au sol)
+        if (m_isGrounded && m_gravityEffect.y < 0)
+        {
+
+            m_gravityEffect.y = -1f;
+        }
 
         // Faire quelque chose pour le sprint a l'arrière
         Vector2 move = playerInput.Player.Move.ReadValue<Vector2>();
 
-        m_characterController.transform.Rotate(0, move.x * m_rotateSpeed * Time.deltaTime, 0);
-
-        // Pbm mystique le else s'appel tjrs même si la condition du if est vérifié
-        if (m_characterController.isGrounded)
+        // Vérifie si le joueur est au sol pour empecher l'air control et pouvoir suater
+        if (m_isGrounded)
         {
-            //Debug.Log("sol");
+            // Rotation du joueur
+            m_characterController.transform.Rotate(0, move.x * m_rotateSpeed * Time.deltaTime, 0);
 
+            // Calcule du mouvement (a opti car il se fait plusieurs fois)
             movement = transform.forward * move.y * m_speed;
-            
-            ///(m_stamBarre = Mathf.Round(1);) jarrive pas a arrondire la stam </summary>
-            
+
+            //(m_stamBarre = Mathf.Round(1);) jarrive pas a arrondire la stam
+
             //activaction si input.pressed et stam sup à 0
             if (playerInput.Player.Sprint.IsPressed() && m_stamBarre > 0)
             {
@@ -71,29 +93,27 @@ public class PlayerMove : MonoBehaviour
                 movement = transform.forward * move.y * (m_speed * m_speedMulti);
             }
 
-            //activation si !input.pressed = rechargement de la stam
+            //activation si !input.pressed = rechargement de la stam /!\ Temporaire en attendant la mécanique de MANGER /!\
             if (!playerInput.Player.Sprint.IsPressed() && m_stamBarre < 20)
             {
                 m_stamBarre += 5 * Time.deltaTime;
             }
-            
-            //petit bon
+
+            //Ajoute une force opposé via une equation pour sauter quand le jouer est au sol
             if (playerInput.Player.Jump.IsPressed())
             {
-                movement = transform.forward * move.x * (m_speed * m_speedMulti);
-                //Debug.Log("Jump");
+                m_gravityEffect.y = Mathf.Sqrt(m_jumpHeight * -2f * m_gravity);
             }
-
         }
         else
         {
-            m_gravityCalcul = 0;
-            m_gravityCalcul -= m_gravity;
-
-            movement.y = m_gravityCalcul;
-            //Debug.Log("Gravity");
+            // applique la gravité quand le joueur n'est pas au sol
+            m_gravityEffect.y += m_gravity * Time.deltaTime;
         }
 
+        //Application du mouvement
         m_characterController.Move(movement * Time.deltaTime);
+        //Application de la gravité
+        m_characterController.Move(m_gravityEffect * Time.deltaTime);
     }
 }
