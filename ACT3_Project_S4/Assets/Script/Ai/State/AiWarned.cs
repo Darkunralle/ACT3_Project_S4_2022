f1de0563c9e61460e11add186e190e1cd438b197
Aymeric <73876993+Darkunralle@users.noreply.github.com>
2022-04-15 14:23:32 +0200

    Update

---
+++ ACT3_Project_S4/Assets/Script/Ai/State/AiWarned.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiWarned : AiState
+{
+    public AiStateId GetId()
+    {
+        return AiStateId.Warned;
+    }
+    public void Enter(AiAgent agent)
+    {
+    }
+    public void Update(AiAgent agent)
+    {
+    }
+
+    public void Exit(AiAgent agent)
+    {
+    }
+}
---
+++ ACT3_Project_S4/Assets/Script/nomNom.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nomNom : MonoBehaviour
+{
+
+    public GameObject parent;
+    
+    private PlayerMove m_player;
+    public void OnTriggerEnter(Collider player)
+    {
+        //check les layers
+        Debug.Log("fdd");
+        parent.SetActive(false);
+
+        if (player.name == "Player")
+        {
+            m_player = player.GetComponent<PlayerMove>();
+            m_player.setStam(20);
+        }
+    }
+} 
+
