694228cd5960a5d431c51cec6b45da3e85d38feb
Tristan-hub-prog <76900319+Tristan-hub-prog@users.noreply.github.com>
2022-05-24 16:21:55 +0200

    up

--- ACT3_Project_S4/Assets/4-Script/1-Ai/StateMachine/AiAgent.cs
+++ ACT3_Project_S4/Assets/4-Script/1-Ai/StateMachine/AiAgent.cs
@@ -19,6 +19,8 @@ public class AiAgent : MonoBehaviour
 
     public switchToPatrol switchstate;
 
+    public AudioSource detection;
+
 
     // Start is called before the first frame update
     void Start()
