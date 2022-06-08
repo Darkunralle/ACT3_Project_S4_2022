d8c8202af58e46bf56a5571ec5911feef6512bb2
nanu <laura.vassal.gd@gmail.com>
2022-06-08 10:11:45 +0200

    up

--- ACT3_Project_S4/Assets/4-Script/checkVideo.cs
+++ ACT3_Project_S4/Assets/4-Script/checkVideo.cs
@@ -11,26 +11,34 @@ public class checkVideo : MonoBehaviour
     public GameObject button;
     public GameObject text;
 
+    AsyncOperation asyncOperation;
+
     void Start()
     {
         loadingStat = 0;
         button.SetActive(false);
         text.SetActive(false);
+        asyncOperation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!asyncOperation.isDone )
+        {
+            asyncOperation.allowSceneActivation = false;
+        }
+        
         sliderLoading.value = loadingStat / 67;
         Debug.Log(loadingStat);
         if(loadingStat < 135) {loadingStat += Time.deltaTime;}
-        if(loadingStat > 10) { text.SetActive(true); if (Input.GetKeyDown("space")) { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); } ; }
+        if(loadingStat > 10) { text.SetActive(true); if (Input.GetKeyDown("space")) { asyncOperation.allowSceneActivation = true; } ; }
         if(loadingStat >= 135) { sliderLoading.gameObject.SetActive(false); button.SetActive(true); text.SetActive(false); }
     }
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        asyncOperation.allowSceneActivation = true;
         Cursor.lockState = CursorLockMode.Locked;
     }
 }
